df2978a7bf2810822b62c2ac4ad8032e0497cc71
J Carlos Rojas <113724270+J-CarlosRojas@users.noreply.github.com>
2025-07-10 13:33:36 -0500

    Animacion de MaskSplitText

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+import MaskSplitText from './components/animations/MaskSplitText'
+
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-  <h1 class="text-4xl font-bold underline">
-    Hello world!
-  </h1>
+      <div>
+        <MaskSplitText text="Guajillo y chile" />
+      </div>
     </>
   )
 }
